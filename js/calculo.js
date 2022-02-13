b925842b782cd70b63b7884f67164dce25fda60f
Jonathan <52434574+JonathanSaan@users.noreply.github.com>
2022-02-13 19:11:38 -0300

    Update calculo.js

--- js/calculo.js
+++ js/calculo.js
@@ -1,17 +1,38 @@
-let h3 = document.querySelector('h3');
-
-const number_buttons = document.querySelectorAll('[data-number]');
-const delete_buttons = document.querySelector('[data-delete]');
-const operator_buttons = document.querySelectorAll('[data-operator]');
-const equals_button = document.querySelector('[data-equals]');
-
-for (const numberButton of number_buttons) {
-  numberButton.addEventListener("click", () => {
-     h3[numberButton].innerHTML += number_buttons.value;
-  });
+function getInputValue() {
+  let input = event.target.innerText
+  printValue(input);
 }
 
-/*equals_button.addEventListener("click", function() {
-  event.preventDefault();
-  h3.innerHTML += `teste`
-});*/
+let buttons = document.getElementsByTagName('button');
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].setAttribute('onclick', 'getInputValue()');
+}
+
+function printValue(val) {
+  let outRespost = document.querySelector("p");
+  let currentRespost = outRespost.innerHTML;
+  
+  let out = document.querySelector("h3");
+  let current = out.innerHTML;
+  if (out.innerHTML == "0") {
+    if (val != "[name='DEL']") {
+      out.innerHTML = "";
+      out.innerHTML += val;
+    }
+  } else {
+    if (val == "[name='DEL']") {
+      out.innerText = current.slice(0, -1);
+      if (out.innerHTML.length <= 1) {
+        out.innerHTML = "0";
+      }
+    }
+    if (val != "[name='DEL']" && val != "=") {
+      out.innerHTML += val;
+    }
+    if (val == "=") {
+      let res = outRespost.innerHTML;
+      outRespost.innerHTML = eval(res);
+    }
+  }
+
+}
