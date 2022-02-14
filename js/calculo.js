71d97798f177f017de33c92442d93c2d593dbb12
Jonathan <52434574+JonathanSaan@users.noreply.github.com>
2022-02-14 12:34:11 -0300

    Update calculo.js

--- js/calculo.js
+++ js/calculo.js
@@ -1,7 +1,7 @@
 function getInputValue() {
-  let input = event.target.innerText
+  let input = event.target.innerHTML;
   printValue(input);
-}
+};
 
 let buttons = document.getElementsByTagName('button');
 for (var i = 0; i < buttons.length; i++) {
@@ -9,29 +9,28 @@ for (var i = 0; i < buttons.length; i++) {
 }
 
 function printValue(val) {
-  let outRespost = document.querySelector("p");
-  let currentRespost = outRespost.innerHTML;
-  
-  let out = document.querySelector("h3");
+  let out = document.querySelector("#numbers").value;
   let current = out.innerHTML;
+  
   if (out.innerHTML == "0") {
-    if (val != "[name='DEL']") {
+    if (val != [id="del"]) {
       out.innerHTML = "";
       out.innerHTML += val;
     }
   } else {
-    if (val == "[name='DEL']") {
-      out.innerText = current.slice(0, -1);
+    if (val == [id="del"]) {
+      out.innerHTML = current.slice(0, -1);
       if (out.innerHTML.length <= 1) {
         out.innerHTML = "0";
       }
     }
-    if (val != "[name='DEL']" && val != "=") {
+    if (val != [id="del"] && val != "=") {
       out.innerHTML += val;
     }
     if (val == "=") {
-      let res = outRespost.innerHTML;
-      outRespost.innerHTML = eval(res);
+      let res = out.innerHTML;
+      out.innerHTML = eval(res);
+      out.innerHTML = " ";
     }
   }
 
